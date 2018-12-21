36691cd798bb6b9f26ef17a030777dcb9d07b049
njleonzhang <scream3616@sina.com>
2018-12-21 11:07:53 +0800

    inject style

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
     create: function(codefundId) {
       return function(hook, vm) {
         hook.ready(function() {
-          window.DocsifyCodefund.injectCodeFundContainer();
+          window.DocsifyCodefund.injectCodeFundStyle();
           window.DocsifyCodefund.injectCodeFundScript(codefundId);
+        })
+
+        hook.doneEach(function () {
+          window.DocsifyCodefund.injectCodeFundContainer();
         })
       }
     },
@@ -25,6 +29,20 @@
       script.src = "https://codefund.app/properties/" + codefundId + "/funder.js?template=docsify"
       script.async = "async"
       document.body.appendChild(script)
+    },
+
+    injectCodeFundStyle() {
+      var style = document.createElement('style');
+      window.DocsifyCodefund.injectCodeFundContainer();
+      var css = '#cf span.cf-wrapper { background-color: transparent!important } #cf a.cf-text { color: #444!important; font-weight: 400!important; font-size: 10px!important }';
+
+      style.type = 'text/css';
+      if (style.styleSheet){
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      document.head.appendChild(style);
     }
   }
 })(window)
